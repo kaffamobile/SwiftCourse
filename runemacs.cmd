fa76ad5bf4f294f2cc37287eba0fa3c0c218bc83
Thijs Brobbel <thijsbrobbel@gmail.com>
2013-01-21 08:16:52 +0100

    Changes default Emacs location to local one.

--- runemacs.cmd
+++ runemacs.cmd
@@ -1,4 +1,5 @@
 @echo off
 if "%HOME%"      == "" set HOME=P:\UT\SwiftCourse
-if "%EMACSROOT%" == "" set EMACSROOT=S:\realworld emacs lt\
+:: if "%EMACSROOT%" == "" set EMACSROOT=S:\realworld emacs lt\
+if "%EMACSROOT%" == "" set EMACSROOT=C:\worksource\emacs\
 "%EMACSROOT%\bin\%~n0.exe" %*
